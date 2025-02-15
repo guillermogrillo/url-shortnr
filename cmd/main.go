712816a8aab0efbddb34ce89ca24f08dd3d4e359
guillermo grillo <guillermo.grillo88@gmail.com>
2025-02-15 14:55:37 -0300

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"urlshortn/pkg/api"
+	"urlshortn/pkg/hash"
+	"urlshortn/pkg/storage"
+	"urlshortn/pkg/token"
+)
+
+const (
+	appName      = "shortn"
+	defaultEpoch = "2010-11-04T00:00:00Z" //this seems to be twitter's default epoch. Using the same
+)
+
+func main() {
+	os.Exit(runApp(appName, os.Args[1:]...))
+}
+
+func runApp(name string, args ...string) int {
+	port := getEnvVarOrDefault("PORT", "8080")
+	fmt.Println("Starting http server on port " + port)
+
+	redisAddr := getEnvVarOrDefault("REDIS_ADDR", "localhost:6379")
+	redisPassword := getEnvVarOrDefault("REDIS_PASSWORD", "")
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+
+	tokenGen := token.NewSnowflakeTokenGenerator(defaultEpoch, logger)
+
+	urlTokenHasher := hash.NewUrlTokenHash(logger)
+
+	urlStore := storage.NewRedisStore(redisAddr, redisPassword, logger)
+
+	urlHandler := api.NewUrlHandler(tokenGen, urlTokenHasher, urlStore, logger)
+
+	http.HandleFunc("/shortn", func(w http.ResponseWriter, r *http.Request) {
+		urlHandler.ShortenUrl(w, r)
+	})
+	http.HandleFunc("/shortn/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			urlHandler.GetLongUrl(w, r)
+		case http.MethodDelete:
+			urlHandler.DeleteShortenUrl(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
+		log.Fatal(err)
+		return 1
+	}
+
+	return 0
+}
+
+func getEnvVarOrDefault(key string, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
